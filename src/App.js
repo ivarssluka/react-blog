6722b1360be19e9c6baa70cbcc386f330b0712fe
Ivarock <ivarssluka@gmail.com>
2024-10-02 18:32:00 +0300

    404 added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -12,13 +13,16 @@ function App() {
         <div className="content">
           <Switch>
             <Route exact path="/">
-            <Home />
+              <Home />
             </Route>
             <Route path="/create">
-            <Create />
+              <Create />
             </Route>
             <Route path="/blogs/:id">
-            <BlogDetails />
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
--- src/BlogList.js
+++ src/BlogList.js
@@ -3,17 +3,16 @@ import { Link } from "react-router-dom";
 const BlogList = ({ blogs }) => {
   return (
     <div className="blog-list">
-      {blogs.map(blog => (
+      {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <Link to={`/blogs/${blog.id}`}>
-          <h2>{ blog.title }</h2>
-          <p>Written by {blog.author}</p>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
           </Link>
-
         </div>
       ))}
     </div>
   );
-}
+};
 
 export default BlogList;
